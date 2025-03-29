e98169b3d2c479d2fc79d1f651307c43c14d8541
Max Wells <maxwells.pro@proton.me>
2025-03-29 12:22:46 +0400

    multi_select_tags.rs: Add inline JS for TagItem.

--- 1-LEPTOS-UI-LABS/app/src/components/ui/multi_select_tags.rs
+++ 1-LEPTOS-UI-LABS/app/src/components/ui/multi_select_tags.rs
@@ -6,7 +6,7 @@ use crate::utils::utils::Utils;
 mod components {
     use super::*;
     div! {MultiSelectTagsSearch, "search flex flex-wrap gap-3 w-[400px] min-h-[48px] p-3 border border-input rounded-lg"}
-    clx! {MultiSelectTags, div, "tags flex flex-wrap gap-3 p-3 rounded-2xl border border-input w-[400px]"}
+    clx! {TagsRoot, div, "tags flex flex-wrap gap-3 p-3 rounded-2xl border border-input w-[400px]"}
     clx! {RootItem, button, "flex items-center gap-2 p-2 rounded-lg cursor-pointer text-muted-foreground border-none"}
 }
 
@@ -22,6 +22,17 @@ pub fn TagItem(children: Children) -> impl IntoView {
     );
 
     view! {
-        <RootItem style=transition_name>{children()} <span class="display-none">X</span></RootItem>
+        <RootItem
+            onclick="const tag = event.target.closest('button'); if (tag) { document.startViewTransition(() => { document.querySelector('.search').appendChild(tag); }); }"
+            style=transition_name
+        >
+            {children()}
+            <span class="display-none">X</span>
+        </RootItem>
     }
+}
+
+#[component]
+pub fn MultiSelectTags(children: Children) -> impl IntoView {
+    view! { <TagsRoot>{children()}</TagsRoot> }
 }
--- 1-LEPTOS-UI-LABS/public/components/tags_animated.js
+++ 1-LEPTOS-UI-LABS/public/components/tags_animated.js
@@ -2,15 +2,6 @@ const search = document.querySelector('.search');
 
 const tagsContainer = document.querySelector('.tags');
 
-tagsContainer.addEventListener('click', (e) => {
-  const tag = e.target.closest('button');
-  if (tag) {
-    document.startViewTransition(() => {
-      search.appendChild(tag);
-    });
-  }
-});
-
 search.addEventListener('click', (e) => {
   const span = e.target.closest('span');
   if (span) {
