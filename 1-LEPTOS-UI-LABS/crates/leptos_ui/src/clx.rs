a903a778f40cb48800889d150e8ea774f82f9b7f
Max Wells <maxwells.pro@proton.me>
2025-03-29 15:24:13 +0400

    Simplify Utils in clx.rs

--- 1-LEPTOS-UI-LABS/crates/leptos_ui/src/clx.rs
+++ 1-LEPTOS-UI-LABS/crates/leptos_ui/src/clx.rs
@@ -1,11 +1,7 @@
-use std::{
-    collections::hash_map::DefaultHasher,
-    hash::{Hash, Hasher},
-    sync::atomic::{AtomicUsize, Ordering},
-};
-
 pub use leptos::prelude::*;
 pub use tw_merge::*;
+
+pub use crate::utils::Utils;
 
 /// A macro that creates a component with tailwind class merging
 ///
@@ -127,25 +123,6 @@ macro_rules! div {
 /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
 /*                     ✨ FUNCTIONS ✨                        */
 /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
-
-pub struct Utils;
-
-impl Utils {
-    pub fn use_random_id() -> String {
-        let mut hasher = DefaultHasher::new();
-
-        static COUNTER: AtomicUsize = AtomicUsize::new(1);
-        let counter = COUNTER.fetch_add(1, Ordering::SeqCst);
-        counter.hash(&mut hasher);
-
-        format!("_gen_id_{}", hasher.finish())
-    }
-
-    pub fn use_random_transition_name() -> String {
-        let random_id = Utils::use_random_id();
-        format!("view-transition-name: {}", random_id)
-    }
-}
 
 #[macro_export]
 macro_rules! transition {
