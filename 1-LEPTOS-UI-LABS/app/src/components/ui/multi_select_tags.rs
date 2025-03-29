81cae9f91b012bed6c319b22bf1b644371a5c8f2
Max Wells <maxwells.pro@proton.me>
2025-03-29 10:59:34 +0400

    multi_select_tags.rs: Add Tailwind classes.

--- 1-LEPTOS-UI-LABS/app/src/components/ui/multi_select_tags.rs
+++ 1-LEPTOS-UI-LABS/app/src/components/ui/multi_select_tags.rs
@@ -3,8 +3,8 @@ use leptos_ui::{clx, div};
 
 mod components {
     use super::*;
-    div! {MultiSelectTagsSearch, "search"}
-    clx! {MultiSelectTags, div, "tags"}
+    div! {MultiSelectTagsSearch, "search flex flex-wrap gap-3 w-[400px] min-h-[48px] p-3 border border-gray-300 rounded-lg text-base"}
+    clx! {MultiSelectTags, div, "tags flex flex-wrap gap-3 p-3 bg-white rounded-2xl border border-gray-300 w-[400px]"}
 }
 
 pub use components::*;
