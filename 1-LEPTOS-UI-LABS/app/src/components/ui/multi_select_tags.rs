a1a3d6ef9728d821fb013b3eaac2f58d3e90d330
Max Wells <maxwells.pro@proton.me>
2025-03-29 11:33:37 +0400

    Simplify CSS

--- 1-LEPTOS-UI-LABS/app/src/components/ui/multi_select_tags.rs
+++ 1-LEPTOS-UI-LABS/app/src/components/ui/multi_select_tags.rs
@@ -3,9 +3,9 @@ use leptos_ui::{clx, div};
 
 mod components {
     use super::*;
-    div! {MultiSelectTagsSearch, "search flex flex-wrap gap-3 w-[400px] min-h-[48px] p-3 border border-gray-300 rounded-lg text-base"}
-    clx! {MultiSelectTags, div, "tags flex flex-wrap gap-3 p-3 bg-white rounded-2xl border border-gray-300 w-[400px]"}
-    clx! {RootItem, button, "p-2 bg-gray-200 rounded-lg text-base text-gray-800 cursor-pointer border-none font-medium flex items-center gap-2"}
+    div! {MultiSelectTagsSearch, "search flex flex-wrap gap-3 w-[400px] min-h-[48px] p-3 border border-input rounded-lg"}
+    clx! {MultiSelectTags, div, "tags flex flex-wrap gap-3 p-3 rounded-2xl border border-input w-[400px]"}
+    clx! {RootItem, button, "flex items-center gap-2 p-2 rounded-lg cursor-pointer text-muted-foreground border-none"}
 }
 
 pub use components::*;
