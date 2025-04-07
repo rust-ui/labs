2554bbe42cb10f0ec9cbbf8ce421f7fba789efe2
Max Wells <maxwells.pro@proton.me>
2025-04-07 22:44:15 +0400

    navbar.rs: Add Gsap intro + force full load of the page for the demo to load properly.

--- 1-LEPTOS-UI-LABS/app/src/shared/components/navbar.rs
+++ 1-LEPTOS-UI-LABS/app/src/shared/components/navbar.rs
@@ -5,8 +5,14 @@ use super::{reactive_indicator::ReactiveIndicator, theme_toggle::ThemeToggle};
 #[component]
 pub fn Navbar() -> impl IntoView {
     view! {
-        <div class="flex justify-between p-4">
-            <a href="/">"HOME 🧪"</a>
+        <div class="flex justify-between p-4 z-50 sticky top-0 bg-background">
+            <div class="flex gap-2">
+                <a href="/">"HOME 🧪"</a>
+                // Need full reload of the page for the animation to load properly
+                <a href="#" onclick="window.location.href='/gsap-intro'; return false;">
+                    "Gsap intro"
+                </a>
+            </div>
 
             <div class="flex items-center gap-4">
                 <ReactiveIndicator />
