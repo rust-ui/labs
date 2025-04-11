25cce5e7bdb50eb34527d10fabdfb1f46e5b5dc6
Max Wells <maxwells.pro@proton.me>
2025-04-11 06:35:11 +0400

    (refacto) gsap_intro: Remove `.css` file and use Tailwind instead.

--- 1-LEPTOS-UI-LABS/app/src/gsap_intro_page.rs
+++ 1-LEPTOS-UI-LABS/app/src/gsap_intro_page.rs
@@ -1,84 +1,46 @@
 use leptos::prelude::*;
-use leptos_meta::Stylesheet;
 
 #[component]
 pub fn GsapIntroPage() -> impl IntoView {
     view! {
-        <Stylesheet href="/components/gsap_intro.css" />
+        <style>
+            {"::-webkit-scrollbar {
+            display: none;
+            }
+            
+            .img__holder {
+            clip-path: polygon(37.5% 20%, 62.5% 20%, 62.5% 80%, 37.5% 80%);
+            }
+            "}
+        </style>
 
-        <div class="h-full mainDiv overscroll-none">
+        <div class="h-full mainDiv overscroll-none font-inte">
 
             <div class="fixed flex flex-row w-full transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 header__scroll z-2">
                 <div class="flex flex-1 letters">
-                    <div>"T"</div>
-                    <div>"A"</div>
-                    <div>"I"</div>
-                    <div>"L"</div>
+                    <div class="flex-1 text-center text-[18vw]">"T"</div>
+                    <div class="flex-1 text-center text-[18vw]">"A"</div>
+                    <div class="flex-1 text-center text-[18vw]">"I"</div>
+                    <div class="flex-1 text-center text-[18vw]">"L"</div>
                 </div>
-                <div class="flex flex-1 letters">
-                    <div>"W"</div>
-                    <div>"I"</div>
-                    <div>"N"</div>
-                    <div>"D"</div>
+                <div class="flex flex-1 text-center text-[18vw] letters">
+                    <div class="flex-1 text-center text-[18vw]">"W"</div>
+                    <div class="flex-1 text-center text-[18vw]">"I"</div>
+                    <div class="flex-1 text-center text-[18vw]">"N"</div>
+                    <div class="flex-1 text-center text-[18vw]">"D"</div>
                 </div>
             </div>
             <div class="fixed top-0 w-full min-h-screen website-content">
-                <div class="img__holder relative w-full h-[100vh] top-0 bg-white">
+                <div class="img__holder relative w-full h-[100vh] top-0 bg-white rotate-[30deg]">
                     <img
-                        class="object-cover w-full h-full"
+                        class="object-cover w-full h-full relative scale-[2]"
                         src="https://images.unsplash.com/photo-1700403322391-f1cd144394cb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                         alt=""
                     />
                 </div>
 
-                <div class="relative p-1 text-white bg-black -w-full content__holder">
-                    <div class="row">
-                        <h1 class="font-normal tracking-tight uppercase text-8xl">History</h1>
-                    </div>
-                    <div class="row">
-                        <div class=" w-[200px] h-[275px]">"Text"</div>
-                    </div>
-
-                    <div class="row">
-                        <p class="w-1/2 text-base font-normal leading-relaxed ">
-                            In the age of knights and castles, master sculptors skillfully
-                            carved stones and marbles with great artistry. From mountains and
-                            forests, the raw materials arose to be shaped according to their
-                            vision.
-                        </p>
-                        <p>
-                            In the age of knights and castles, master sculptors skillfully
-                            carved stones and marbles with great artistry. From mountains and
-                            forests, the raw materials arose to be shaped according to their
-                            vision.
-                        </p>
-                        <p>
-                            In the age of knights and castles, master sculptors skillfully
-                            carved stones and marbles with great artistry. From mountains and
-                            forests, the raw materials arose to be shaped according to their
-                            vision.
-                        </p>
-                        <p>
-                            In the age of knights and castles, master sculptors skillfully
-                            carved stones and marbles with great artistry. From mountains and
-                            forests, the raw materials arose to be shaped according to their
-                            vision.
-                        </p>
-                        <p>
-                            In the age of knights and castles, master sculptors skillfully
-                            carved stones and marbles with great artistry. From mountains and
-                            forests, the raw materials arose to be shaped according to their
-                            vision.
-                        </p>
-                    </div>
-                    <div class="row">
-                        <p>
-                            These artisans, wielding hammers and chisels, brought to life the
-                            mysteries of faith and ancient tales in stone. Every stroke, with
-                            precision and reverence, narrated stories of virtues, triumphs, and
-                            sometimes tragic human tales.
-                        </p>
-                    </div>
+                <div class="relative bg-muted/60 p-1 -w-full content__holder min-h-[100vh] grid place-content-center">
+                    <h2 class="font-bold text-4xl">"Your content goes here 😄"</h2>
                 </div>
             </div>
 
