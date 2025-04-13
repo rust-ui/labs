1900c102cdb26c1dc3c064691672de2cf9abf544
Max Wells <maxwells.pro@proton.me>
2025-04-13 15:22:19 +0400

    (feat) Initial implementation for Query

--- 1-LEPTOS-UI-LABS/app/src/all_demos_page.rs
+++ 1-LEPTOS-UI-LABS/app/src/all_demos_page.rs
@@ -1,48 +1,68 @@
 use leptos::prelude::*;
+use leptos_ui::{a, clx};
 
-use crate::__demos__::{
-    _process_to_follow::ProcessToFollow, demo_carousel::DemoCarousel, demo_drawer::DemoDrawer,demo_cards_gradient::DemoCardsGradient,
-};
+use crate::__demos__::_process_to_follow::ProcessToFollow;
+use crate::__demos__::demo_cards_gradient::DemoCardsGradient;
+use crate::__demos__::demo_carousel::DemoCarousel;
+use crate::__demos__::demo_drawer::DemoDrawer;
+use crate::shared::utils::params::QueryUtils;
 
-// * 1️⃣ ---------- Put the name of your demo here 👇 ----------
-const DEMO_1: &str = "--- Process to Follow ---";
+const DEMO_1: &str = "Process to follow";
 const DEMO_2: &str = "Carousel";
 const DEMO_3: &str = "Drawer";
 const DEMO_4: &str = "Gradient Card";
+const DEMO_5: &str = "Slot_5";
+const DEMO_6: &str = "Slot_6";
+const DEMO_7: &str = "Slot_7";
+const DEMO_8: &str = "Slot_8";
 
-// * 2️⃣ ---------- Put the name of your demo here 👆 ----------
+// TODO. Use format! macro.
+
+#[component]
+pub fn AllDemosPage() -> impl IntoView {
+    clx! {Sidenav, div, "flex flex-col h-full gap-4 bg-neutral-500 w-[300px]"}
+    a! {SidenavLink, "px-4 py-2 hover:bg-neutral-600"}
+
+    view! {
+        <div class="flex gap-4 p-2 mx-4">
+            <Sidenav>
+                <SidenavLink href="?demo=Process to follow">{DEMO_1}</SidenavLink>
+                <SidenavLink href="?demo=Carousel">{DEMO_2}</SidenavLink>
+                <SidenavLink href="?demo=Drawer">{DEMO_3}</SidenavLink>
+                <SidenavLink href="?demo=Gradient Card">{DEMO_4}</SidenavLink>
+                <SidenavLink href="?demo=Slot_5">{DEMO_5}</SidenavLink>
+                <SidenavLink href="?demo=Slot_6">{DEMO_6}</SidenavLink>
+                <SidenavLink href="?demo=Slot_7">{DEMO_7}</SidenavLink>
+                <SidenavLink href="?demo=Slot_8">{DEMO_8}</SidenavLink>
+            </Sidenav>
+
+            <RenderComponentFromQuery />
+        </div>
+    }
+}
 
 /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
 /*                     ✨ FUNCTIONS ✨                        */
 /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
 #[component]
-pub fn AllDemosPage() -> impl IntoView {
-    let demo = RwSignal::new(DEMO_1.to_string());
-
-    // *  ------------------ 2️⃣ Put your Component here 👇 ------------------
-    let render_demo = move || match demo.get().as_str() {
-        DEMO_1 => view! { <ProcessToFollow /> }.into_any(),
-        DEMO_2 => view! { <DemoCarousel /> }.into_any(),
-        DEMO_3 => view! { <DemoDrawer /> }.into_any(),
-        DEMO_4 => view! { <DemoCardsGradient /> }.into_any(),
-        _ => view! {}.into_any(),
-    };
-    // *  ------------------ 2️⃣ Put your Component here 👆 ------------------
+pub fn RenderComponentFromQuery() -> impl IntoView {
+    let demo_query = QueryUtils::extract_demo();
 
     view! {
-        <div class="flex gap-4 p-2 mx-4">
-            <div class="flex flex-col h-full gap-4 bg-neutral-500 w-[300px]">
-                // * 3️⃣ ---------- Put the name of your demo here 👇 ----------
-                <button on:click=move |_| { demo.set(DEMO_1.into()) }>{DEMO_1}</button>
-                <button on:click=move |_| { demo.set(DEMO_2.into()) }>{DEMO_2}</button>
-                <button on:click=move |_| { demo.set(DEMO_3.into()) }>{DEMO_3}</button>
-                <button on:click=move |_| { demo.set(DEMO_4.into()) }>{DEMO_4}</button>
-
-            // * 3️⃣ ---------- Put the name of your demo here 👆 ----------
+        <div class="flex flex-col gap-4 w-full">
+            <div class="w-full">
+                {move || {
+                    let demo = demo_query();
+                    match demo.as_str() {
+                        DEMO_1 => view! { <ProcessToFollow /> }.into_any(),
+                        DEMO_2 => view! { <DemoCarousel /> }.into_any(),
+                        DEMO_3 => view! { <DemoDrawer /> }.into_any(),
+                        DEMO_4 => view! { <DemoCardsGradient /> }.into_any(),
+                        _ => view! { <p>"Select a component to display"</p> }.into_any(),
+                    }
+                }}
             </div>
-
-            <div class="w-full">{render_demo}</div>
         </div>
     }
 }
