6f5ac51e2c02cf69a6fbed93384bf730b44dddcc
Max Wells <maxwells.pro@proton.me>
2025-04-16 19:57:34 +0400

    (feat) Add new 👉-TODO-*

---
+++ 👉👉👉👉-TODOS-👉👉👉/👉-TODO-BOTTOM-BAR-AWWARDS/script.js
@@ -0,0 +1,25 @@
+const PARENT = document.querySelector('#parent');
+const CHILDREN = PARENT.querySelectorAll('*');
+const BUTTON = document.querySelector('#nav-toggle');
+const LINK_LI = document.querySelectorAll('li:has(a)');
+
+BUTTON.addEventListener('click', (e) => {
+  PARENT.classList.toggle('active');
+});
+
+LINK_LI.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    LINK_LI.forEach(el2 => {
+      if (el !== el2) {
+        el2.classList.remove('active');
+      }
+    });
+    el.classList.toggle('active');
+  })
+});
+
+document.addEventListener('mouseover', (e) => {
+  if (!Array.from(CHILDREN).includes(e.target) && (e.target !== PARENT)) {
+    PARENT.classList.remove('active');
+  }
+});
