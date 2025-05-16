acebb7b46e9f67fcc3b50600a94ce1a4f1365e06
Max Wells <maxwells.pro@proton.me>
2025-05-16 23:13:05 +0400

    card_removal.rs: Code cosmetics.

--- app/src/__demos__/demo_animated_card_removal.rs
+++ app/src/__demos__/demo_animated_card_removal.rs
@@ -3,68 +3,44 @@ use leptos_ui::utils::Utils;
 use leptos_ui::{clx, transition};
 
 #[component]
-fn DeleteButton() -> impl IntoView {
-    view! {
-        <DeleteButtonWrapper class="delete-btn">
-            <img
-                src="https://upload.wikimedia.org/wikipedia/commons/7/7d/Trash_font_awesome.svg"
-                alt="close button"
-                class="color-white"
-            />
-            <span class="border-0 clip-[rect(1px,1px,1px,1px)] clip-path-[inset(50%)] h-px -m-px overflow-hidden p-0 absolute w-px whitespace-nowrap">
-                Delete
-            </span>
-        </DeleteButtonWrapper>
-    }
-}
-
-struct ItemData {
-    color: String,
-}
-
-#[component]
 pub fn DemoAnimatedCardRemoval() -> impl IntoView {
-    let items = vec![
-        ItemData {
-            color: "bg-[tan]".to_string(),
-        },
-        ItemData {
-            color: "bg-[khaki]".to_string(),
-        },
-        ItemData {
-            color: "bg-[thistle]".to_string(),
-        },
-        ItemData {
-            color: "bg-[lightblue]".to_string(),
-        },
-        ItemData {
-            color: "bg-[lightgreen]".to_string(),
-        },
-    ];
-
     view! {
-        <Container>
-            {
-                items.into_iter().map(|item| {
-                    view! {
-                        <Item class=format!("{}", item.color)>
-                            <DeleteButton />
-                        </Item>
-                    }
-                }).collect::<Vec<_>>()
-            }
-        </Container>
+        <div class="flex justify-center w-full gap-8 max-w-[1000px]">
+           <CardItemDemo />
+           <CardItemDemo />
+           <CardItemDemo />
+           <CardItemDemo />
+           <CardItemDemo />
+        </div>
 
         <script src="/components/cards_removal.js"></script>
     }
 }
 
+/*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+/*                     ✨ FUNCTIONS ✨                        */
+/*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+#[component]
+fn CardItemDemo() -> impl IntoView {
+    view! {
+        <CardItem>
+            <DeleteButtonWrapper class="delete-btn">
+                <span>"🗑️"</span>
+            </DeleteButtonWrapper>
+        </CardItem>
+    }
+}
+
+/*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+/*                     ✨ FUNCTIONS ✨                        */
+/*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
 mod components {
     use super::*;
 
-    clx! {Container, div, "flex justify-center w-full gap-8 max-w-[1000px]"}
-    transition! {Item, li, "w-full aspect-[2/3] block relative rounded-xl max-w-[220px]"}
-    clx! {DeleteButtonWrapper, button, "absolute -bottom-3 -right-3 w-12 h-12 p-2 border-4 rounded-full bg-[steelblue] text-white"}
+    transition! {CardItem, li, "w-full aspect-[2/3] block relative rounded-xl max-w-[220px] bg-neutral-400"}
+    clx! {DeleteButtonWrapper, button, "cursor-pointer absolute -bottom-3 -right-3 size-12 p-2 border-4 rounded-full bg-neutral-100"}
 }
 
 pub use components::*;
