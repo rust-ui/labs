935c02e35720a5778515061a20970b9d8056cd1a
Max Wells <maxwells.pro@proton.me>
2025-05-18 12:36:28 +0400

    (refacto): Code cosmetics.

--- app/src/__demos__/_todo__use_tailwind_css.rs
+++ app/src/__demos__/_todo__use_tailwind_css.rs
@@ -5,27 +5,29 @@ use crate::shared::components::card::{Card, CardContent, CardDescription, CardHe
 #[component]
 pub fn Todo__UseTailwindCss() -> impl IntoView {
     view! {
-        <div class="flex flex-col items-center justify-center gap-6 py-8">
-            <h1 class="text-2xl font-bold">
+        <Card class="gap-6 pb-8 mx-auto bg-orange-50 w-fit">
+            <h1 class="text-2xl font-bold text-center">
                 "👉 TODO"
             </h1>
 
-            <div class="flex flex-col gap-2">
-                <p>"1. Use Tailwind CSS instead of plain CSS (see Stylesheet)."</p>
-                <p>"2. Use `clx!` macro to create components."</p>
-                <p>"3. When it's done, replace '👉' by '✔️' in `all_demos_page.rs`."</p>
-            </div>
+            <div class="flex items-center gap-6">
+                <div class="flex flex-col gap-2">
+                    <p>"1. Use Tailwind CSS instead of plain CSS (see Stylesheet `src`)."</p>
+                    <p>"2. Use `clx!` macro to create components."</p>
+                    <p>"3. When it's done, replace '👉' by '✔️' in `all_demos.rs`."</p>
+                </div>
 
-            <Card class="max-w-[400px] bg-green-50 p-2">
-                <CardContent>
-                    <CardHeader>
-                        <CardTitle>"TIPS 💡"</CardTitle>
-                        <CardDescription>"1. Use `.cursor/rules` to go much faster. "</CardDescription>
-                        <CardDescription>"2. If you don't find the equivalent in Tailwind, leave as it is."</CardDescription>
-                        <CardDescription>"3. Use `clx!` ONLY if it's relevant to the component itself.."</CardDescription>
-                    </CardHeader>
-                </CardContent>
-            </Card>
-        </div>
+                <Card class="max-w-[400px] bg-green-50 p-2">
+                    <CardContent>
+                        <CardHeader>
+                            <CardTitle>"TIPS 💡"</CardTitle>
+                            <CardDescription>"1. Use `.cursor/rules` to go much faster. "</CardDescription>
+                            <CardDescription>"2. If you don't find the equivalent in Tailwind, leave as it is."</CardDescription>
+                            <CardDescription>"3. Use `clx!` ONLY if it's relevant to the component itself.."</CardDescription>
+                        </CardHeader>
+                    </CardContent>
+                </Card>
+            </div>
+        </Card>
     }
 }
