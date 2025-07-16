76293b5cad05ca71d0ddcdf296c25dbd224ecb2b
Max Wells <maxwells.pro@proton.me>
2025-07-16 07:11:23 +0400

    Step 5 Complete: Replace generic CSS class names with descriptive names

--- 👉👉👉👉-TODOS-👉👉👉/JS_MENU_INTERACTION/script.js
+++ 👉👉👉👉-TODOS-👉👉👉/JS_MENU_INTERACTION/script.js
@@ -1,5 +1,5 @@
-const widget = document.querySelector(".widget");
-const toggle = document.querySelector(".toggle");
+const widget = document.querySelector(".menu__widget");
+const toggle = document.querySelector(".menu__toggle__button");
 
 toggle.addEventListener("click", () => {
   widget.classList.toggle("active");
