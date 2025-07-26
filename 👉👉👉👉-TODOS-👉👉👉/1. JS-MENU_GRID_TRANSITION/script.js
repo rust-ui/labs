caf039a9b8bb48ffd251491b7c2b5c603a373916
Max Wells <maxwells.pro@proton.me>
2025-07-26 19:14:17 +0400

    renaming

--- 👉👉👉👉-TODOS-👉👉👉/1. JS-MENU_GRID_TRANSITION/script.js
+++
@@ -1,8 +0,0 @@
-const navigation = document.querySelector(".grid__menu__navigation");
-const close = document.querySelector(".grid__menu__close");
-navigation.onclick = () => {
-  navigation.classList.add("active");
-};
-close.onclick = () => {
-  navigation.classList.remove("active");
-};
--- 👉👉👉👉-TODOS-👉👉👉/✔️ JS_MENU_INTERACTION/script.js
+++
@@ -1,6 +0,0 @@
-const widget = document.querySelector(".menu__widget");
-const toggle = document.querySelector(".menu__toggle__button");
-
-toggle.addEventListener("click", () => {
-  widget.classList.toggle("active");
-});
