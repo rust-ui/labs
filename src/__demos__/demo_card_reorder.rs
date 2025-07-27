5ad34ce71cb4b7e3d7fbd26201bbb07dde2493e2
Max Wells <maxwells.pro@proton.me>
2025-07-27 14:18:48 +0400

    use clx.

--- src/__demos__/demo_card_reorder.rs
+++ src/__demos__/demo_card_reorder.rs
@@ -1,7 +1,10 @@
 use leptos::prelude::*;
+use leptos_ui::clx;
 
 #[component]
 pub fn DemoCardReorder() -> impl IntoView {
+    clx! {Card, div, "grid place-content-center text-2xl bg-gray-300 rounded card w-[20vw] max-w-20 aspect-[1/1.6]"}
+
     view! {
         <style>
             r#"
@@ -16,34 +19,18 @@ pub fn DemoCardReorder() -> impl IntoView {
         </style>
 
         <div class="flex flex-row gap-4 justify-center p-4">
-            <div
-                class="grid place-content-center text-2xl bg-gray-300 rounded card w-[20vw] max-w-20 aspect-[1/1.6]"
-                id="card-1"
-                style="view-transition-name: card-1; view-transition-class: card;"
-            >
+            <Card id="card-1" style="view-transition-name: card-1; view-transition-class: card;">
                 1
-            </div>
-            <div
-                class="grid place-content-center text-2xl bg-gray-300 rounded card w-[20vw] max-w-20 aspect-[1/1.6]"
-                id="card-2"
-                style="view-transition-name: card-2; view-transition-class: card;"
-            >
+            </Card>
+            <Card id="card-2" style="view-transition-name: card-2; view-transition-class: card;">
                 2
-            </div>
-            <div
-                class="grid place-content-center text-2xl bg-gray-300 rounded card w-[20vw] max-w-20 aspect-[1/1.6]"
-                id="card-3"
-                style="view-transition-name: card-3; view-transition-class: card;"
-            >
+            </Card>
+            <Card id="card-3" style="view-transition-name: card-3; view-transition-class: card;">
                 3
-            </div>
-            <div
-                class="grid place-content-center text-2xl bg-gray-300 rounded card w-[20vw] max-w-20 aspect-[1/1.6]"
-                id="card-4"
-                style="view-transition-name: card-4; view-transition-class: card;"
-            >
+            </Card>
+            <Card id="card-4" style="view-transition-name: card-4; view-transition-class: card;">
                 4
-            </div>
+            </Card>
         </div>
 
         <div class="flex justify-center">
