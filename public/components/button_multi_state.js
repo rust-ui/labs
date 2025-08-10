c9d3f0b4dd719848e78a212f4ef1b3e8ccb64f95
Max Wells <maxwells.pro@proton.me>
2025-08-10 20:29:42 +0400

    Add TODOS

--- public/components/button_multi_state.js
+++ public/components/button_multi_state.js
@@ -1,19 +1,16 @@
-const states = {
-    idle: "Do some hard work",
-    sending:
-      '<img aria-hidden="true" width="24" height="24" src="https://assets.codepen.io/2585/ring-resize.svg" alt="" /> working...',
-    done: "Done!",
-  };
-  
-  demo.onclick = () => {
-    setState("sending");
-    setTimeout(() => setState("done"), 4000);
-    setTimeout(() => setState("idle"), 6000);
-  };
-  
-  function setState(state) {
-    if (!document.startViewTransition) demo.innerHTML = states[state];
-    else document.startViewTransition(() => (demo.innerHTML = states[state]));
-  }
-  //# sourceURL=pen.js
-  
+const STATES = {
+  idle: "Do some hard work",
+  working: "⏳ working...",
+  done: "Done! ✅",
+};
+
+demo.onclick = () => {
+  setState("working");
+  setTimeout(() => setState("done"), 2000);
+  setTimeout(() => setState("idle"), 4000);
+};
+
+function setState(state) {
+  if (!document.startViewTransition) demo.innerHTML = STATES[state];
+  else document.startViewTransition(() => (demo.innerHTML = STATES[state]));
+}
