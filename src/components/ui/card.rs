834146b9377e63a2a88f1448dae61c9ce8005caa
Max Wells <maxwells.pro@proton.me>
2025-09-09 17:30:46 +0200

    Update card.rs

--- src/components/ui/card.rs
+++ src/components/ui/card.rs
@@ -3,12 +3,14 @@ use leptos_ui::clx;
 
 mod components {
     use super::*;
-    clx! {Card, div, "rounded-lg border bg-card shadow", "p-4 w-full"}
-    clx! {CardHeader, div, "flex flex-col space-y-1.5"}
-    clx! {CardTitle, h3, "text-2xl font-semibold leading-none tracking-tight"}
-    clx! {CardContent, div, "pt-4"}
-    clx! {CardDescription, p, "text-sm", "text-muted-foreground"}
-    clx! {CardFooter, footer, "mt-4", "flex items-center justify-end"}
+    clx! {Card, div, "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm"}
+    clx! {CardHeader, div, "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6"}
+    clx! {CardTitle, h3, "leading-none font-semibold"}
+    clx! {CardContent, div, "px-6"}
+    clx! {CardDescription, p, "text-muted-foreground text-sm"}
+    clx! {CardFooter, footer, "flex items-center px-6 [.border-t]:pt-6"}
+
+    clx! {CardAction, div, "col-start-2 row-span-2 row-start-1 self-start justify-self-end"}
 }
 
 pub use components::*;
