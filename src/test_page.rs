875893214b9101a7af695c98f6e3a8e68bc6498c
Max Wells <maxwells.pro@proton.me>
2025-09-14 15:55:12 +0200

    Add IconsWrapper.

--- src/test_page.rs
+++ src/test_page.rs
@@ -1,16 +1,19 @@
 use icons::{AArrowDownAnimate, HeartAnimate, PlusAnimate};
 use leptos::prelude::*;
+use leptos_ui::clx;
 
 #[component]
 pub fn TestPage() -> impl IntoView {
+    clx! {IconsWrapper, div, "group", "flex justify-center items-center p-2 rounded-md transition-colors duration-200 cursor-pointer select-none  hover:bg-accent"}
+
     view! {
         <div>
             <h1>Test Page</h1>
 
             <div class="flex gap-8 justify-center items-center m-0 min-h-screen font-sans">
-                <div class="flex justify-center items-center p-2 rounded-md transition-colors duration-200 cursor-pointer select-none hover:bg-accent">
+                <IconsWrapper>
                     <AArrowDownAnimate />
-                </div>
+                </IconsWrapper>
 
                 <HeartAnimate />
                 <PlusAnimate />
