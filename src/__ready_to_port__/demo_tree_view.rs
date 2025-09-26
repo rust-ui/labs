002ddb082881270e216996331a60b356442265d9
Max Wells <maxwells.pro@proton.me>
2025-09-26 10:14:48 +0200

    Move components in clx.

--- src/__ready_to_port__/demo_tree_view.rs
+++ src/__ready_to_port__/demo_tree_view.rs
@@ -6,6 +6,8 @@ use crate::utils::hooks::use_random::use_random_id;
 mod components {
     use super::*;
     clx! {Tree, div, "rounded-md border not-prose bg-neutral-50 w-[240px] border-neutral-200"}
+    clx! {FolderTrigger, summary, "flex flex-row gap-2 items-center py-1.5 px-2 w-full text-sm rounded-md cursor-pointer [&_svg:not([class*='size-'])]:size-4 hover:bg-accent hover:text-accent-foreground"}
+    clx! {FileList, ul, "flex flex-col pl-2 ml-6 relative before:content-[''] before:absolute before:-left-2 before:top-0 before:bottom-0 before:border-l before:border-neutral-300"}
 }
 
 pub use components::*;
@@ -14,24 +16,24 @@ pub use components::*;
 #[component]
 pub fn DemoTreeView() -> impl IntoView {
     view! {
-            <Tree>
-                <Folder name="app" open=true>
-                    <File name="layout.tsx" checked=true />
+        <Tree>
+            <Folder name="app" open=true>
+                <File name="layout.tsx" checked=true />
+                <File name="page.tsx" />
+                <File name="globals.css" />
+                <Folder name="dashboard">
                     <File name="page.tsx" />
-                    <File name="globals.css" />
-                    <Folder name="dashboard">
-                        <File name="page.tsx" />
-                        <File name="layout.tsx" />
-                    </Folder>
+                    <File name="layout.tsx" />
                 </Folder>
+            </Folder>
 
-                <Folder name="components">
-                    <File name="button.tsx" />
-                    <File name="input.tsx" />
-                </Folder>
+            <Folder name="components">
+                <File name="button.tsx" />
+                <File name="input.tsx" />
+            </Folder>
 
-                 <File name="package.json" />
-            </Tree>
+            <File name="package.json" />
+        </Tree>
     }
 }
 
@@ -49,19 +51,18 @@ pub fn Folder(
     children: Children,
 ) -> impl IntoView {
     view! {
-        <details data-name="Folder" class="flex flex-col open:[&>summary>svg:first-child]:rotate-90" prop:open=open>
-            <summary
-                data-name="FolderTrigger"
-                class="flex flex-row gap-2 items-center py-1.5 px-2 w-full text-sm rounded-md cursor-pointer [&_svg:not([class*='size-'])]:size-4 hover:bg-accent hover:text-accent-foreground"
-            >
+        <details
+            data-name="Folder"
+            class="flex flex-col open:[&>summary>svg:first-child]:rotate-90"
+            prop:open=open
+        >
+            <FolderTrigger>
                 <ChevronRight class="transition-transform duration-200 ease-in-out origin-center" />
                 <FolderIcon />
                 <span>{name}</span>
-            </summary>
+            </FolderTrigger>
 
-            <ul data-name="FileList" class="flex flex-col pl-2 ml-6 relative before:content-[''] before:absolute before:-left-2 before:top-0 before:bottom-0 before:border-l before:border-neutral-300">
-                {children()}
-            </ul>
+            <FileList>{children()}</FileList>
         </details>
     }
 }
@@ -75,7 +76,13 @@ pub fn File(
 
     view! {
         <li data-name="File" class="flex flex-row [details_&]:-ml-4">
-            <input id=target_id.clone() type="radio" name="file-selection" class="sr-only peer" checked=checked />
+            <input
+                id=target_id.clone()
+                type="radio"
+                name="file-selection"
+                class="sr-only peer"
+                checked=checked
+            />
             <label
                 for=target_id
                 class="flex flex-row gap-2 items-center py-1.5 px-2 ml-3 w-full text-sm rounded-md cursor-pointer focus:outline-none [>_svg]:size-4 peer-checked:bg-neutral-200 peer-checked:font-medium hover:peer-checked:bg-neutral-200 hover:bg-accent hover:text-accent-foreground"
