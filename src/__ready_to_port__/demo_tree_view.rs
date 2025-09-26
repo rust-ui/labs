b76634a00a4ff5de13b3e107ab76b810c1de0ceb
Max Wells <maxwells.pro@proton.me>
2025-09-26 09:21:47 +0200

    Add File

--- src/__ready_to_port__/demo_tree_view.rs
+++ src/__ready_to_port__/demo_tree_view.rs
@@ -1,4 +1,4 @@
-use icons::{ChevronRight, Folder as FolderIcon, File};
+use icons::{ChevronRight, Folder as FolderIcon, File as FileIcon};
 use leptos::prelude::*;
 use leptos_ui::clx;
 
@@ -6,6 +6,7 @@ mod components {
     use super::*;
     clx! {Tree, div, "rounded-md border not-prose bg-neutral-50 w-[240px] border-neutral-200"}
     clx! {Folder, details, "flex flex-col group/tree-folder"}
+    clx! {FileList, ul, "flex flex-col pl-2 ml-6"}
 }
 
 pub use components::*;
@@ -20,66 +21,11 @@ pub fn DemoTreeView() -> impl IntoView {
                     <FolderTrigger>
                         <span>"app"</span>
                     </FolderTrigger>
-                    <ul data-name="FileList" class="flex flex-col pl-2 ml-6">
-                        <li
-                            data-name="File"
-                            class="flex flex-row -ml-4 border-l border-neutral-300"
-                        >
-                            <input
-                                id="file-layout"
-                                type="radio"
-                                name="file-selection"
-                                class="sr-only peer"
-                                checked=true
-                            />
-                            <label
-                                for="file-layout"
-                                class="flex flex-row gap-2 items-center py-1.5 px-2 ml-3 w-full text-sm rounded-md cursor-pointer focus:outline-none [>_svg]:size-4 peer-checked:bg-neutral-200 peer-checked:font-medium hover:bg-accent hover:text-accent-foreground"
-                                tabindex="0"
-                            >
-                                <File class="size-4" />
-                                <span>"layout.tsx"</span>
-                            </label>
-                        </li>
-                        <li
-                            data-name="File"
-                            class="flex flex-row -ml-4 border-l border-neutral-300"
-                        >
-                            <input
-                                id="file-page"
-                                type="radio"
-                                name="file-selection"
-                                class="sr-only peer"
-                            />
-                            <label
-                                for="file-page"
-                                class="flex flex-row gap-2 items-center py-1.5 px-2 ml-3 w-full text-sm rounded-md cursor-pointer focus:outline-none [>_svg]:size-4 peer-checked:bg-neutral-200 peer-checked:font-medium hover:bg-accent hover:text-accent-foreground"
-                                tabindex="0"
-                            >
-                                <File class="size-4" />
-                                <span>"page.tsx"</span>
-                            </label>
-                        </li>
-                        <li
-                            data-name="File"
-                            class="flex flex-row -ml-4 border-l border-neutral-300"
-                        >
-                            <input
-                                id="file-global"
-                                type="radio"
-                                name="file-selection"
-                                class="sr-only peer"
-                            />
-                            <label
-                                for="file-global"
-                                class="flex flex-row gap-2 items-center py-1.5 px-2 ml-3 w-full text-sm rounded-md cursor-pointer focus:outline-none [>_svg]:size-4 peer-checked:bg-neutral-200 peer-checked:font-medium hover:bg-accent hover:text-accent-foreground"
-                                tabindex="0"
-                            >
-                                <File class="size-4" />
-                                <span>"global.css"</span>
-                            </label>
-                        </li>
-                    </ul>
+                    <FileList>
+                        <File name="layout.tsx" group_name="file-selection" target_id="file-layout" checked=true />
+                        <File name="page.tsx" group_name="file-selection" target_id="file-page" />
+                        <File name="globals.css" group_name="file-selection" target_id="file-globals" />
+                    </FileList>
                 </Folder>
                 <details
                     data-name="Folder"
@@ -134,10 +80,10 @@ pub fn DemoTreeView() -> impl IntoView {
                             />
                             <label
                                 for="file-button"
-                                class="flex flex-row gap-2 items-center py-1.5 px-2 ml-3 w-full text-sm rounded-md cursor-pointer focus:outline-none [>_svg]:size-4 peer-checked:bg-neutral-200 peer-checked:font-medium hover:bg-accent hover:text-accent-foreground"
+                                class="flex flex-row gap-2 items-center py-1.5 px-2 ml-3 w-full text-sm rounded-md cursor-pointer focus:outline-none [>_svg]:size-4 peer-checked:bg-neutral-200 peer-checked:font-medium hover:peer-checked:bg-neutral-200 hover:bg-accent hover:text-accent-foreground"
                                 tabindex="0"
                             >
-                                <File class="size-4" />
+                                <FileIcon class="size-4" />
                                 <span>"button.tsx"</span>
                             </label>
                         </li>
@@ -153,10 +99,10 @@ pub fn DemoTreeView() -> impl IntoView {
                             />
                             <label
                                 for="file-input"
-                                class="flex flex-row gap-2 items-center py-1.5 px-2 ml-3 w-full text-sm rounded-md cursor-pointer focus:outline-none [>_svg]:size-4 peer-checked:bg-neutral-200 peer-checked:font-medium hover:bg-accent hover:text-accent-foreground"
+                                class="flex flex-row gap-2 items-center py-1.5 px-2 ml-3 w-full text-sm rounded-md cursor-pointer focus:outline-none [>_svg]:size-4 peer-checked:bg-neutral-200 peer-checked:font-medium hover:peer-checked:bg-neutral-200 hover:bg-accent hover:text-accent-foreground"
                                 tabindex="0"
                             >
-                                <File class="size-4" />
+                                <FileIcon class="size-4" />
                                 <span>"input.tsx"</span>
                             </label>
                         </li>
@@ -174,7 +120,7 @@ pub fn DemoTreeView() -> impl IntoView {
                         class="flex flex-row gap-2 items-center py-1.5 px-2 w-full text-sm rounded-md cursor-pointer focus:outline-none [>_svg]:size-4 peer-checked:bg-neutral-200 peer-checked:font-medium hover:bg-accent hover:text-accent-foreground"
                         tabindex="0"
                     >
-                        <File class="size-4" />
+                        <FileIcon class="size-4" />
                         <span>"package.json"</span>
                     </label>
                 </div>
@@ -200,6 +146,28 @@ pub fn FolderTrigger(children: Children) -> impl IntoView {
             <FolderIcon />
             {children()}
         </summary>
+    }
+}
+
+#[component]
+pub fn File(
+    #[prop(into)] name: &'static str,
+    #[prop(into)] group_name: &'static str,
+    #[prop(into)] target_id: &'static str,
+    #[prop(default = false)] checked: bool,
+) -> impl IntoView {
+    view! {
+        <li data-name="File" class="flex flex-row -ml-4 border-l border-neutral-300">
+            <input id=target_id type="radio" name=group_name class="sr-only peer" checked=checked />
+            <label
+                for=target_id
+                class="flex flex-row gap-2 items-center py-1.5 px-2 ml-3 w-full text-sm rounded-md cursor-pointer focus:outline-none [>_svg]:size-4 peer-checked:bg-neutral-200 peer-checked:font-medium hover:peer-checked:bg-neutral-200 hover:bg-accent hover:text-accent-foreground"
+                tabindex="0"
+            >
+                <FileIcon class="size-4" />
+                <span>{name}</span>
+            </label>
+        </li>
     }
 }
 
